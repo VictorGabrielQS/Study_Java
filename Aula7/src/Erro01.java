c87631f92b811ce7d7d6f3842bb035e3004aa580
Victor2412 <victor.git24@gmail.com>
2025-06-25 06:46:29 -0300

    Aula 7

---
+++ Aula7/src/Erro01.java
@@ -0,0 +1,6 @@
+public class Erro01 {
+    public static void main(String[] args) {
+   /*     System.out.println("Hello, World!")
+   * Gera um erro de sintaxe por faltar ; no final da chaves   * */
+    }
+}
